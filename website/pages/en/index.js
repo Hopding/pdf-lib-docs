05136f335b1bf3ead6c74cb0eca7773464b62903
Andrew Dillon <andrew.dillon.j@gmail.com>
2020-09-14 16:37:33 -0500

    Add create and fill form examples

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -148,6 +148,178 @@ async function modifyPdf() {
 }
 ${pre}`;
 
+const createFormSnippet = `${pre}js
+import { PDFDocument } from 'pdf-lib'
+
+async function createForm() {
+  const pdfDoc = await PDFDocument.create()
+
+  const page = pdfDoc.addPage([550, 750])
+
+  const form = pdfDoc.getForm()
+
+  page.drawText('Enter your favorite superhero:', { x: 50, y: 700, size: 20 })
+
+  const superheroField = form.createTextField('favorite.superhero')
+  superheroField.setText('One Punch Man')
+  superheroField.addToPage(page, { x: 55, y: 640 })
+
+  page.drawText('Select your favorite rocket:', { x: 50, y: 600, size: 20 })
+
+  page.drawText('Falcon Heavy', { x: 120, y: 560, size: 18 })
+  page.drawText('Saturn IV', { x: 120, y: 500, size: 18 })
+  page.drawText('Delta IV Heavy', { x: 340, y: 560, size: 18 })
+  page.drawText('Space Launch System', { x: 340, y: 500, size: 18 })
+
+  const rocketField = form.createRadioGroup('favorite.rocket')
+  rocketField.addOptionToPage('Falcon Heavy', page, { x: 55, y: 540 })
+  rocketField.addOptionToPage('Saturn IV', page, { x: 55, y: 480 })
+  rocketField.addOptionToPage('Delta IV Heavy', page, { x: 275, y: 540 })
+  rocketField.addOptionToPage('Space Launch System', page, { x: 275, y: 480 })
+  rocketField.select('Saturn IV')
+
+  page.drawText('Select your favorite gundams:', { x: 50, y: 440, size: 20 })
+
+  page.drawText('Exia', { x: 120, y: 400, size: 18 })
+  page.drawText('Kyrios', { x: 120, y: 340, size: 18 })
+  page.drawText('Virtue', { x: 340, y: 400, size: 18 })
+  page.drawText('Dynames', { x: 340, y: 340, size: 18 })
+
+  const exiaField = form.createCheckBox('gundam.exia')
+  const kyriosField = form.createCheckBox('gundam.kyrios')
+  const virtueField = form.createCheckBox('gundam.virtue')
+  const dynamesField = form.createCheckBox('gundam.dynames')
+
+  exiaField.addToPage(page, { x: 55, y: 380 })
+  kyriosField.addToPage(page, { x: 55, y: 320 })
+  virtueField.addToPage(page, { x: 275, y: 380 })
+  dynamesField.addToPage(page, { x: 275, y: 320 })
+
+  exiaField.check()
+  dynamesField.check()
+
+  page.drawText('Select your favorite planet*:', { x: 50, y: 280, size: 20 })
+
+  const planetsField = form.createDropdown('favorite.planet')
+  planetsField.addOptions(['Venus', 'Earth', 'Mars', 'Pluto'])
+  planetsField.select('Pluto')
+  planetsField.addToPage(page, { x: 55, y: 220 })
+
+  page.drawText('Select your favorite person:', { x: 50, y: 180, size: 18 })
+
+  const personField = form.createOptionList('favorite.person')
+  personField.addOptions([
+    'Julius Caesar',
+    'Ada Lovelace',
+    'Cleopatra',
+    'Aaron Burr',
+    'Mark Antony',
+  ])
+  personField.select('Ada Lovelace')
+  personField.addToPage(page, { x: 55, y: 70 })
+
+  page.drawText(\`* Pluto should be a planet too!\`, { x: 15, y: 15, size: 15 })
+
+  const pdfBytes = await pdfDoc.save()
+}
+${pre}`;
+
+const fillFormSnippet = `${pre}js
+import { PDFDocument } from 'pdf-lib'
+
+async function fillForm() {
+  const formUrl = 'https://pdf-lib.js.org/assets/dod_character.pdf'
+  const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer())
+
+  const marioUrl = 'https://pdf-lib.js.org/assets/small_mario.png'
+  const marioImageBytes = await fetch(marioUrl).then(res => res.arrayBuffer())
+
+  const emblemUrl = 'https://pdf-lib.js.org/assets/mario_emblem.png'
+  const emblemImageBytes = await fetch(emblemUrl).then(res => res.arrayBuffer())
+
+  const pdfDoc = await PDFDocument.load(formPdfBytes)
+
+  const marioImage = await pdfDoc.embedPng(marioImageBytes)
+  const emblemImage = await pdfDoc.embedPng(emblemImageBytes)
+
+  const form = pdfDoc.getForm()
+
+  const nameField = form.getTextField('CharacterName 2')
+  const ageField = form.getTextField('Age')
+  const heightField = form.getTextField('Height')
+  const weightField = form.getTextField('Weight')
+  const eyesField = form.getTextField('Eyes')
+  const skinField = form.getTextField('Skin')
+  const hairField = form.getTextField('Hair')
+
+  const alliesField = form.getTextField('Allies')
+  const factionField = form.getTextField('FactionName')
+  const backstoryField = form.getTextField('Backstory')
+  const traitsField = form.getTextField('Feat+Traits')
+  const treasureField = form.getTextField('Treasure')
+
+  const characterImageField = form.getButton('CHARACTER IMAGE')
+  const factionImageField = form.getButton('Faction Symbol Image')
+
+  nameField.setText('Mario')
+  ageField.setText('24 years')
+  heightField.setText(\`5' 1"\`)
+  weightField.setText('196 lbs')
+  eyesField.setText('blue')
+  skinField.setText('white')
+  hairField.setText('brown')
+
+  characterImageField.setImage(marioImage)
+
+  alliesField.setText(
+    [
+      \`Allies:\`,
+      \`  • Princess Daisy\`,
+      \`  • Princess Peach\`,
+      \`  • Rosalina\`,
+      \`  • Geno\`,
+      \`  • Luigi\`,
+      \`  • Donkey Kong\`,
+      \`  • Yoshi\`,
+      \`  • Diddy Kong\`,
+      \`\`,
+      \`Organizations:\`,
+      \`  • Italian Plumbers Association\`,
+    ].join('\\n'),
+  )
+
+  factionField.setText(\`Mario's Emblem\`)
+
+  factionImageField.setImage(emblemImage)
+
+  backstoryField.setText(
+    [
+      \`Mario is a fictional character in the Mario video game franchise, \`,
+      \`owned by Nintendo and created by Japanese video game designer Shigeru \`,
+      \`Miyamoto. Serving as the company's mascot and the eponymous \`,
+      \`protagonist of the series, Mario has appeared in over 200 video games \`,
+      \`since his creation. Depicted as a short, pudgy, Italian plumber who \`,
+      \`resides in the Mushroom Kingdom, his adventures generally center \`,
+      \`upon rescuing Princess Peach from the Koopa villain Bowser. His \`,
+      \`younger brother and sidekick is Luigi.\`,
+    ].join('\\n'),
+  )
+
+  traitsField.setText(
+    [
+      \`Mario can use three basic three power-ups:\`,
+      \`  • the Super Mushroom, which causes Mario to grow larger\`,
+      \`  • the Fire Flower, which allows Mario to throw fireballs\`,
+      \`  • the Starman, which gives Mario temporary invincibility\`,
+    ].join('\\n'),
+  )
+
+  treasureField.setText(['• Gold coins', '• Treasure chests'].join('\\n'))
+
+  const pdfBytes = await pdfDoc.save()
+}
+${pre}`;
+
 const copyPagesSnippet = `${pre}js
 import { PDFDocument } from 'pdf-lib'
 
@@ -588,8 +760,18 @@ class Index extends React.Component {
                     </translate>
                   ),
                 },
+                {
+                  title: <translate>Fill Forms</translate>,
+                  content: (
+                    <translate>
+                      Create new forms or fill and read existing fields. Check
+                      boxes, buttons, radio groups, dropdowns, option lists, and
+                      text fields are all supported.
+                    </translate>
+                  ),
+                },
               ]}
-              layout="threeColumn"
+              layout="fourColumn"
             />
           </Container>
           <Container>
@@ -724,6 +906,20 @@ class Index extends React.Component {
                 />
                 <MarkdownBlock>{modifyDocumentSnippet}</MarkdownBlock>
                 <Pdf url="/assets/modify_document.pdf" />
+
+                <ExampleHeader
+                  title="Create Form"
+                  // jsFiddleUrl="https://jsfiddle.net/Hopding/rxwsc8f5/13/"
+                />
+                <MarkdownBlock>{createFormSnippet}</MarkdownBlock>
+                <Pdf url="/assets/create_form.pdf" />
+
+                <ExampleHeader
+                  title="Fill Form"
+                  // jsFiddleUrl="https://jsfiddle.net/Hopding/rxwsc8f5/13/"
+                />
+                <MarkdownBlock>{fillFormSnippet}</MarkdownBlock>
+                <Pdf url="/assets/fill_form.pdf" />
 
                 <ExampleHeader
                   title="Copy Pages"
